ca7dfabbecb1c7bb854fc1a275de74e7614bb706
eunbileeme <pask2202@gmail.com>
2024-04-09 18:37:54 +0900

    [feat] Google OAuth 로그인 구현

---
+++ src/main/java/com/yoonNeun/MyDreamPartner/domain/user/application/OAuthService.java
@@ -0,0 +1,82 @@
+package com.yoonNeun.MyDreamPartner.domain.user.application;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.core.env.Environment;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+// TODO 일단 구현 후, 블로그에 정리
+@Service
+public class OAuthService {
+
+    private final Environment env;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public OAuthService(Environment env) {
+        this.env = env;
+    }
+
+    public void socialLogin(String code, String registrationId) {
+        String accessToken = getAccessToken(code, registrationId);
+        JsonNode userResourceNode = getUserResource(accessToken, registrationId);
+        System.out.println("userResourceNode = " + userResourceNode);
+
+        String id = userResourceNode.get("id").asText();
+        String email = userResourceNode.get("email").asText();
+        String nickname = userResourceNode.get("name").asText();
+
+        System.out.println("id = " + id);
+        System.out.println("email = " + email);
+        System.out.println("nickname = " + nickname);
+    }
+
+    private String getAccessToken(String authorizationCode, String registrationId) {
+        String clientId = env.getProperty("oauth2." + registrationId + ".client-id");
+        String clientSecret = env.getProperty("oauth2." + registrationId + ".client-secret");
+        String redirectUri = env.getProperty("oauth2." + registrationId + ".redirect-uri");
+        String tokenUri = env.getProperty("oauth2." + registrationId + ".token-uri");
+
+        if (tokenUri == null) {
+            throw new IllegalArgumentException("Token URI must not be null");
+        }
+
+        // 하나의 Key와 하나 이상의 value로 이루어진 리스트
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("code", authorizationCode);
+        params.add("client_id", clientId);
+        params.add("client_secret", clientSecret);
+        params.add("redirect_uri", redirectUri);
+        params.add("grant_type", "authorization_code");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
+
+        ResponseEntity<JsonNode> responseNode = restTemplate.exchange(tokenUri, HttpMethod.POST, entity, JsonNode.class);
+
+        return Optional.ofNullable(responseNode.getBody())
+                .map(body -> body.get("access_token").asText())
+                .orElse(null);
+    }
+
+    private JsonNode getUserResource(String accessToken, String registrationId) {
+        String resourceUri = env.getProperty("oauth2." + registrationId + ".resource-uri");
+
+        if (resourceUri == null) {
+            throw new IllegalArgumentException("Resource URI must not be null");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(resourceUri, HttpMethod.GET, entity, JsonNode.class).getBody();
+    }
+}
