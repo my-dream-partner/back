91c42ee27f1f2d9a89d476c3820b3a5a0559371f
eunbileeme <pask2202@gmail.com>
2024-05-30 00:06:37 +0900

    [refactor] @SpringBootTest(classes = MyDreamPartnerApplicationTests.class) 추가

--- src/test/java/yoonNeun/MyDreamPartner/MyDreamPartnerApplicationTests.java
+++ src/test/java/yoonNeun/MyDreamPartner/MyDreamPartnerApplicationTests.java
@@ -3,7 +3,7 @@ package yoonNeun.MyDreamPartner;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MyDreamPartnerApplicationTests.class)
 class MyDreamPartnerApplicationTests {
 
 	@Test
