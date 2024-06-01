656d99089bce2b1e3e6781815e6854327372f011
eunbileeme <pask2202@gmail.com>
2024-06-01 18:16:44 +0900

    [feat] 설문 안내 페이지 (난이도 선택 후) 가능 구현

--- src/main/java/com/yoonNeun/MyDreamPartner/domain/survey/infrastructure/SurveyRepositoryCustom.java
+++ src/main/java/com/yoonNeun/MyDreamPartner/domain/survey/infrastructure/SurveyRepositoryCustom.java
@@ -1,9 +1,16 @@
 package com.yoonNeun.MyDreamPartner.domain.survey.infrastructure;
 
 import com.yoonNeun.MyDreamPartner.domain.survey.domain.Survey;
+import com.yoonNeun.MyDreamPartner.domain.survey.domain.SurveyDescription;
 
 import java.util.List;
 
 public interface SurveyRepositoryCustom {
-    List<Survey> findSurveyDetails();
+    List<Survey> findSurveyIntroductionDetails();
+
+    SurveyDescription findSurveyDescription(String type);
+
+    List<String> findCategoryNamesByType(String type);
+
+    List<String> findSummariesByType(String type);
 }
--- src/main/java/com/yoonNeun/MyDreamPartner/domain/survey/infrastructure/SurveyRepositoryCustomImpl.java
+++ src/main/java/com/yoonNeun/MyDreamPartner/domain/survey/infrastructure/SurveyRepositoryCustomImpl.java
@@ -3,12 +3,12 @@ package com.yoonNeun.MyDreamPartner.domain.survey.infrastructure;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.yoonNeun.MyDreamPartner.domain.survey.domain.Survey;
+import com.yoonNeun.MyDreamPartner.domain.survey.domain.SurveyDescription;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-// 수동으로 Q class import
 import static com.yoonNeun.MyDreamPartner.domain.type.domain.QType.type;
 import static com.yoonNeun.MyDreamPartner.domain.category.domain.QCategory.category;
 import static com.yoonNeun.MyDreamPartner.domain.typecategory.domain.QTypeCategory.typeCategory;
@@ -20,7 +20,7 @@ public class SurveyRepositoryCustomImpl implements SurveyRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<Survey> findSurveyDetails() {
+    public List<Survey> findSurveyIntroductionDetails() {
         return queryFactory
                 .select(Projections.constructor(Survey.class,
                         type.typeName,
@@ -30,6 +30,40 @@ public class SurveyRepositoryCustomImpl implements SurveyRepositoryCustom {
                 .from(typeCategory)
                 .join(typeCategory.type, type)
                 .join(typeCategory.category, category)
+                .fetch();
+    }
+
+    @Override
+    public SurveyDescription findSurveyDescription(String typeName) {
+        return queryFactory
+                .select(Projections.constructor(SurveyDescription.class,
+                        type.typeName,
+                        type.content,
+                        type.totalScore))
+                .from(typeCategory)
+                .join(typeCategory.type, type)
+                .join(typeCategory.category, category)
+                .where(type.typeName.eq(typeName))
+                .fetchFirst();
+    }
+
+    public List<String> findCategoryNamesByType(String typeName) {
+        return queryFactory
+                .select(category.categoryName)
+                .from(typeCategory)
+                .join(typeCategory.category, category)
+                .join(typeCategory.type, type)
+                .where(type.typeName.eq(typeName))
+                .fetch();
+    }
+
+    public List<String> findSummariesByType(String typeName) {
+        return queryFactory
+                .select(category.summary)
+                .from(typeCategory)
+                .join(typeCategory.category, category)
+                .join(typeCategory.type, type)
+                .where(type.typeName.eq(typeName))
                 .fetch();
     }
 }
