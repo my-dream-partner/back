4454a558f6f01108f1b4b63ba714a887a841abf6
eunbileeme <pask2202@gmail.com>
2024-06-05 19:04:23 +0900

    [feat] 회원가입 기능 구현

---
+++ src/main/java/com/yoonNeun/MyDreamPartner/domain/user/application/UserSignUpService.java
@@ -0,0 +1,23 @@
+package com.yoonNeun.MyDreamPartner.domain.user.application;
+
+import com.yoonNeun.MyDreamPartner.domain.user.infrastructure.UserSignUpRepository;
+import com.yoonNeun.MyDreamPartner.domain.user.domain.UserProfile;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserSignUpService {
+
+    private final UserSignUpRepository userSignUpRepository;
+
+    public void addUserProfile(Integer age, String gender, String position) {
+        UserProfile userProfile = UserProfile.builder()
+                .age(age)
+                .gender(gender)
+                .position(position)
+                .build();
+
+        userSignUpRepository.save(userProfile);
+    }
+}
--- src/main/java/com/yoonNeun/MyDreamPartner/domain/user/domain/UserProfile.java
+++ src/main/java/com/yoonNeun/MyDreamPartner/domain/user/domain/UserProfile.java
@@ -1,6 +1,8 @@
 package com.yoonNeun.MyDreamPartner.domain.user.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,12 +22,17 @@ public class UserProfile {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @NotBlank
     private String gender;
-    private Long age;
+
+    @NotNull
+    private Integer age;
+
+    @NotBlank
     private String position;
 
     @Builder
-    public UserProfile(User user, String gender, Long age, String position) {
+    public UserProfile(User user, String gender, Integer age, String position) {
         this.user = user;
         this.gender = gender;
         this.age = age;
