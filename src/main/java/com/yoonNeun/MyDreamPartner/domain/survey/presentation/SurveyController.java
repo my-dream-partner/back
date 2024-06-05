d99e8692b6652a8ec02d96f89529c5d2cc0d0793
eunbileeme <pask2202@gmail.com>
2024-06-05 21:14:40 +0900

    [refactor] 누락된 검증 어노테이션 추가

--- src/main/java/com/yoonNeun/MyDreamPartner/domain/survey/presentation/SurveyController.java
+++ src/main/java/com/yoonNeun/MyDreamPartner/domain/survey/presentation/SurveyController.java
@@ -5,13 +5,16 @@ import com.yoonNeun.MyDreamPartner.domain.survey.application.SurveyService;
 import com.yoonNeun.MyDreamPartner.domain.survey.domain.dto.Survey;
 import com.yoonNeun.MyDreamPartner.domain.survey.domain.dto.SurveyDescription;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class SurveyController {
@@ -25,7 +28,7 @@ public class SurveyController {
     }
 
     @GetMapping("/survey/description")
-    public SuccessResponse<SurveyDescription> getSurveyDescription(@RequestParam @NotBlank String type) {
+    public SuccessResponse<SurveyDescription> getSurveyDescription(@RequestParam @NotBlank @Size(min = 4, max = 6) String type) {
         SurveyDescription surveyDescription = surveyService.getSurveyDescription(type);
         return new SuccessResponse<>(surveyDescription);
     }
